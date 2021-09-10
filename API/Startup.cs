540a5f0f5f65cf68fce5aed6cd5bfa3aff225984
carissasamaroo <carissasamaroo@gmail.com>
2021-09-09 21:40:38 -0400

    Section 4 completed

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,8 +1,13 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using API.Data;
+using API.Extensions;
+using API.Interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -12,6 +17,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 namespace API
@@ -30,11 +36,27 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DataContext>(options => 
+
+            services.AddApplicationServices(_config);
+
+            //https://docs.microsoft.com/en-us/aspnet/core/security/cors?view=aspnetcore-5.0
+            services.AddCors(options =>
             {
-                options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
+                options.AddDefaultPolicy(
+                    builder =>
+                    {
+                        builder.WithOrigins("https://localhost:4200",
+                                            "http://localhost:4200")
+                                            .AllowAnyHeader()
+                                            .AllowAnyMethod();
+                    });
             });
+
             services.AddControllers();
+
+            services.AddIdentityServices(_config);
+
+            //services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
@@ -54,6 +76,14 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            /*app.UseCors(x => x.AllowAnyHeader()
+            .AllowAnyMethod()
+            .WithOrigins("http://localhost:4200"));*/
+
+            app.UseCors();
+            
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
